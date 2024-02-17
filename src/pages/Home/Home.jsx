74662c6e5ebb0eb795e66e877d0a5ecf0021d277
Aleksey S <94798542+Alsidryid@users.noreply.github.com>
2024-02-17 18:13:31 +0200

    add picture

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import style from './Home.module.css';
-
+import picture from '../../images/2.png';
 const Home = () => {
   return (
     <div className={style.home}>
       <h1>PHONEBOOK</h1>
+      <img className={style.image} src={picture} alt="phone" />
     </div>
   );
 };
